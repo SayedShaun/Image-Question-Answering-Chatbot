6ec1e1e0255f4f0a45e1f23d78ef3c369f07a22c
Shaun <sayedshaun4@gmail.com>
2024-02-05 15:47:57 +0600

    Version 1

---
+++ app.py
@@ -0,0 +1,71 @@
+import io
+import requests
+from PIL import Image
+import matplotlib.pyplot as plt
+import streamlit as st
+from llm import LLM
+import sys
+
+image = Image.open("Image/url_image.png")
+st.set_page_config(page_title="Know Image",page_icon=image)
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.image("Image/title.jpg")
+st.sidebar.title("Paste Image Link")
+input_url = st.sidebar.text_input("")
+st.sidebar.button("Submit", use_container_width=True)
+tab = st.sidebar.radio("", 
+                       ("Image Description",
+                        "Hotel Finder",
+                        "Custom Query")
+                        )
+
+def plot_image(image_url):
+    response = requests.get(image_url)
+    image = Image.open(
+        io.BytesIO(response.content)
+        ).resize((1280, 720))
+    return image
+
+if input_url:
+    st.image(plot_image(input_url))
+else:
+    sys.exit(0)
+
+
+gemini = LLM([input_url])
+
+if tab=="Image Description":
+    response = gemini.describe_image()
+    st.write(f"<span style='color:white'>{response}</span>", unsafe_allow_html=True)
+
+if tab=="Hotel Finder":
+    response = gemini.hotel_finder()
+    st.write(f"<span style='color:white'>{response}</span>", unsafe_allow_html=True)
+    
+if tab == "Custom Query":
+    prompt: str = st.chat_input("Enter Your Question here")
+
+    USER = "user"
+    ASSISTANT = "assistant"
+
+    if prompt:
+        st.chat_message(USER).write(f"USER: {prompt}")
+        response = gemini.custom_query(prompt)
+        st.chat_message(ASSISTANT).write(f"GEMINI: {response}")
+
+
+
+
+
+
+
+
+
---
+++ llm.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
+from llama_index.multi_modal_llms.gemini import GeminiMultiModal
+from llama_index.multi_modal_llms.generic_utils import load_image_urls
+
+
+
+class LLM:
+    def __init__(self, place_image_link) -> None:
+        self.image_link = place_image_link
+        self.model = GeminiMultiModal(model_name="models/gemini-pro-vision")
+        self.image_doc = load_image_urls(self.image_link)
+
+    def describe_image(self):
+        image_doc = load_image_urls(self.image_link)
+        
+        response = self.model.complete(
+            prompt="Describe the Images within 100 words",
+            image_documents=image_doc
+        )
+        return response
+    
+    def hotel_finder(self):
+        response = self.model.complete(
+            prompt="""Where is this place City and Country tell in one word? 
+            Recommend some nearby holel based on this place""",
+            image_documents=self.image_doc
+        )
+        return response
+    
+    def custom_query(self, prompt):
+        response = self.model.complete(
+            prompt=prompt,
+            image_documents=self.image_doc
+        )
+        return response
+
+
+
+
